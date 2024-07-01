dcd372739239ec716548f06b4f1e42aab6ecae7f
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2024-06-30 21:43:00 -0300

    adicionadas chamadas de sistema
    adicionadas apenas as assinaturas de settickets e getpinfo

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -102,6 +102,8 @@ extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
 extern uint64 sys_getcnt(void);
+extern uint64 sys_settickets(void);
+extern uint64 sys_getpinfo(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -128,6 +130,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getcnt]  sys_getcnt,
+[SYS_settickets] sys_settickets,
+[SYS_getpinfo] sys_getpinfo,
 };
 
 void
