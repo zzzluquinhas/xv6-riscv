5ef840fd80fad5f9ac67886485d2ff4f22fd5e44
JuliaGuerreiro <Juliamanu10@gmail.com>
2024-07-11 00:07:49 -0300

    Adição da declaração de tickets

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,6 +91,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int tickets;                 // Número de tickets que este processo contém
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
@@ -105,5 +106,5 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 
-  uint64 syscall_count[22];    // Array que contabiliza o número de vezes que cada syscall foi chamada
+  uint64 syscall_count[22];    // Array que contabiliza o número de vezes que cada syscall foi chamada 
 };
